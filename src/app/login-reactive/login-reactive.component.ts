b01b6aaf41596875bc4aa242305411f6f0d5373b
thiago-anjos <your_email@example.com>
2022-12-11 17:15:26 -0300

    formGrou finished

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { createPasswordStrengthValidator } from "../validadors/password-strength.validator";
 
 @Component({
   selector: "login",
@@ -9,10 +10,15 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 export class LoginReactiveComponent implements OnInit {
   email = new FormControl("", {
     validators: [Validators.required, Validators.email],
+    updateOn: "blur",
   });
 
   password = new FormControl("", {
-    validators: [Validators.required, Validators.minLength(3)],
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      createPasswordStrengthValidator(),
+    ],
   });
 
   form: FormGroup = new FormGroup({
