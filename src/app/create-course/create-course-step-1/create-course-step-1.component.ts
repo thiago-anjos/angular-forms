d5795ca0c5d7b4fc3b0df1959990acf0e5a06717
thiago-anjos <your_email@example.com>
2022-12-17 16:29:18 -0300

    Create a title course validator async wich wait for backend response to let user add title or note

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 import { CoursesService } from "../../services/courses.service";
-import { Observable } from "rxjs";
-import { filter } from "rxjs/operators";
+import { courseTitleValidator } from "../../validadors/course-title.validator";
 
 @Component({
   selector: "create-course-step-1",
@@ -10,9 +9,28 @@ import { filter } from "rxjs/operators";
   styleUrls: ["./create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component implements OnInit {
-  form = this.fb.group({});
+  form = this.fb.group({
+    title: [
+      "",
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(60),
+        ],
+        asyncValidators: [courseTitleValidator(this.course)],
+        updateOn: "blur",
+      },
+    ],
+  });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private course: CoursesService) {}
 
   ngOnInit() {}
+
+  //create a getter to access title property of form
+
+  get courseTitle() {
+    return this.form.controls.title;
+  }
 }
