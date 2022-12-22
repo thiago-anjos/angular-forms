7033c18e0f0ab5b9e6054e6ed31165be017f13c2
thiago-anjos <your_email@example.com>
2022-12-22 19:34:33 -0300

    dinamic fields

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, Validators } from "@angular/forms";
 import { CoursesService } from "../../services/courses.service";
 import { courseTitleValidator } from "../../validadors/course-title.validator";
+import { Observable } from "rxjs";
+
+interface CourseCategory {
+  code: string;
+  description: string;
+}
 
 @Component({
   selector: "create-course-step-1",
@@ -9,6 +15,8 @@ import { courseTitleValidator } from "../../validadors/course-title.validator";
   styleUrls: ["./create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component implements OnInit {
+  first: string = "";
+
   form = this.fb.group({
     title: [
       "",
@@ -23,15 +31,23 @@ export class CreateCourseStep1Component implements OnInit {
       },
     ],
     releaseDateAt: [new Date(), Validators.required],
+    downLoadsAllowed: [false, Validators.requiredTrue],
+    longDescription: ["", [Validators.required, Validators.minLength(6)]],
+    category: ["BEGINNER", Validators.required],
   });
 
   constructor(private fb: FormBuilder, private course: CoursesService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.courseCategories$ = this.course.findCourseCategories();
+  }
 
   //create a getter to access title property of form
 
   get courseTitle() {
     return this.form.controls.title;
   }
+
+  //create a observable to call endpoint
+  courseCategories$: Observable<CourseCategory[]>;
 }
--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,19 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-2',
-  templateUrl: 'create-course-step-2.component.html',
-  styleUrls: ['create-course-step-2.component.scss']
+  selector: "create-course-step-2",
+  templateUrl: "create-course-step-2.component.html",
+  styleUrls: ["create-course-step-2.component.scss"],
 })
 export class CreateCourseStep2Component implements OnInit {
+  form = this.fb.group({
+    courseType: ["premium", Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-
-
-
-  }
-
+  ngOnInit() {}
 }
