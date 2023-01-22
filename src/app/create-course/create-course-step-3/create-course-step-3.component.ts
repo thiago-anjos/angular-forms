46550cf961e94eb901ea65278d5d2c9b58f2a1fa
thiago-anjos <your_email@example.com>
2023-01-22 18:59:14 -0300

    criar formulários dinamicos com formArrya da api do angular - ultimo step

--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,13 +1,31 @@
-import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { FormArray, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-3',
-  templateUrl: 'create-course-step-3.component.html',
-  styleUrls: ['create-course-step-3.component.scss']
+  selector: "create-course-step-3",
+  templateUrl: "create-course-step-3.component.html",
+  styleUrls: ["create-course-step-3.component.scss"],
 })
 export class CreateCourseStep3Component {
+  form = this.fb.group({
+    lessons: this.fb.array([]),
+  });
+  constructor(private fb: FormBuilder) {}
 
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
 
+  addLessons() {
+    const lessonForm = this.fb.group({
+      title: ["", Validators.required],
+      level: ["beginner", Validators.required],
+    });
+
+    this.lessons.push(lessonForm);
+  }
+
+  deleteLesson(lessonIndex) {
+    this.lessons.removeAt(lessonIndex);
+  }
 }
